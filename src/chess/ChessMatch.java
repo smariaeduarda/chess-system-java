5017cfa2796e7c76ba86275454ded486cfd0658a
Maria Eduarda <mariaeduardasilv916@gmail.com>
2023-09-27 09:26:15 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -84,7 +84,8 @@ public class ChessMatch {
 	}
 
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 
